fb6a8a5fa3e15e615f1386ef07ad08ef70e435f2
DV <digvijaykachhawaha0087@gmail.com>
2024-11-30 17:01:36 +0530

    part5

---
+++ part5/promises.js
@@ -0,0 +1,12 @@
+/*   ------ PROMISES
+
+A Promise in JavaScript is a way to handle asynchronous tasks (like fetching data from an API) that may not finish immediately.
+
+Key Points:
+Pending: The task is not yet done.
+Resolved/fulfilled: The task completed successfully.
+Rejected: The task failed.
+
+A Promise is a way to handle tasks that take time, and it lets you deal with success or failure later.
+
+*/
